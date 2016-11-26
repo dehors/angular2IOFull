4f5c7ab0c93d14db8fc4e6707ec4dcf642feec82
slam24 <luismartinez242011@gmail.com>
2016-11-26 14:12:51 -0600

    feat: Routes

---
+++ app/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }   from '@angular/router';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+@Component({
+  selector: 'my-heroes',
+  templateUrl: '../../app/heroes.component.html',
+  styleUrls: [ '../../app/heroes.component.css' ],
+  providers: [HeroService]
+})
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  //Inyectar el HeroService
+  constructor(private router: Router,private heroService: HeroService) { }
+
+  //obtener la informacion
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+gotoDetail(): void {
+  this.router.navigate(['/detail', this.selectedHero.id]);
+}
+}
